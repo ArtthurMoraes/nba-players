6360228e84ec93f08166118f2a8004fbdcff6d08
Arthur Moraes <133591851+ArtthurMoraes@users.noreply.github.com>
2024-09-05 20:04:14 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+function pesquisar() {
+    // Esta função é responsável por realizar uma pesquisa e exibir os resultados na página.
+    
+    // Obtém a seção onde os resultados serão exibidos pelo seu ID.
+    let section = document.getElementById("resultados-pesquisa");
+
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa.
+    let resultados = '';
+
+    // Itera sobre cada elemento do array 'dados' (que não foi mostrado no código).
+    // Para cada elemento, cria um novo elemento HTML 'div' com as informações do dado.
+    for (let dado of dados){ 
+        resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}
+                </p>
+                <a href=${dado.link} target="_blank">Saiba mais</a>
+            </div>
+        `;
+    }
+
+    // Atribui o conteúdo da string 'resultados' ao elemento HTML 'section'.
+    // Isso substitui o conteúdo anterior da seção pelos novos resultados da pesquisa.
+    section.innerHTML = resultados;
+}
---
+++ dados.js
@@ -0,0 +1,112 @@
+let dados = [
+    {
+        titulo: "Michael Jordan",
+        descricao: "Michael Jeffrey Jordan, nascido em 17 de fevereiro de 1963, é amplamente considerado o maior jogador de basquete de todos os tempos. Ele conquistou seis títulos da NBA com o Chicago Bulls e foi eleito o MVP das finais em todas essas ocasiões. Jordan também foi cinco vezes MVP da temporada regular e participou de 14 NBA All-Star Games.",
+        link: "https://pt.wikipedia.org/wiki/Michael_Jordan"
+    },
+
+
+    {
+        titulo: "LeBron James",
+        descricao: "LeBron Raymone James, nascido em 30 de dezembro de 1984, é um dos maiores jogadores de basquete de todos os tempos.Atualmente jogando pelo Los Angeles Lakers, ele conquistou quatro títulos da NBA e foi eleito o MVP das finais em todas essas ocasiões.LeBron também foi quatro vezes MVP da temporada regular e participou de 19 NBA All- Star Games.",
+        link: "https://pt.wikipedia.org/wiki/LeBron_James"
+    },
+
+    {
+        titulo: "Kobe Bryant",
+        descricao: "Kobe Bean Bryant, nascido em 23 de agosto de 1978 e falecido em 26 de janeirode 2020, é considerado um dos maiores jogadores de basquete da história. Ele passou toda suacarreira de 20 anos no Los Angeles Lakers, conquistando cinco títulos da NBA e sendo eleito o MVP das finais duas vezes. Kobe também foi 18 vezes All-Star e MVP da temporada regular em 2008.",
+        link: "https://pt.wikipedia.org/wiki/Kobe_Bryant"
+
+    },
+
+    {
+        titulo: "Kareem Abdul-Jabbar",
+        descricao: "Lew Alcindor, mais conhecido como Kareem Abdul-Jabbar, é um ex-jogador de basquete norte-americano, considerado um dos maiores de todos os tempos. Ele é o maior pontuador da história da NBA e possui o recorde de mais títulos de MVP da temporada regular.",
+        link: "https://pt.wikipedia.org/wiki/Kareem_Abdul-Jabbar"
+    },
+
+    {
+        titulo: "Bill Russell",
+        descricao: "William Felton Russell é um ex-jogador de basquete norte-americano, considerado um dos maiores defensores da história da NBA. Ele conquistou 11 títulos da NBA com o Boston Celtics, um recorde que até hoje permanece invicto.",
+        link: "https://pt.wikipedia.org/wiki/Bill_Russell"
+    },
+    {
+        titulo: "Magic Johnson",
+        descricao: "Earvin Magic Johnson Jr. é um ex-jogador de basquete norte-americano, conhecido por sua habilidade de passar a bola e liderar a equipe. Ele conquistou cinco títulos da NBA com o Los Angeles Lakers e foi um dos principais rivais de Larry Bird.",
+        link: "https://pt.wikipedia.org/wiki/Magic_Johnson"
+    },
+
+
+    {
+        titulo: "Larry Bird",
+        descricao: "Lawrence Joseph Bird é um ex-jogador de basquete norte-americano, conhecido por sua habilidade de arremessar e liderança. Ele conquistou três títulos da NBA com o Boston Celtics e foi um dos principais rivais de Magic Johnson.",
+        link: "https://pt.wikipedia.com/wiki/Larry_Bird"
+    },
+
+    {
+        titulo: "Shaquille O'Neal",
+        descricao: "Shaquille Rashaun O'Neal é um ex-jogador de basquete norte-americano, conhecido por sua força física e domínio na pintura. Ele conquistou quatro títulos da NBA e foi três vezes MVP das finais.",
+        link: "https://pt.wikipedia.org/wiki/Shaquille_O'Neal"
+    },
+
+    {
+        titulo: "Tim Duncan",
+        descricao: "Timothy Theodore Duncan, conhecido como Tim Duncan, é um ex-jogador de basquete norte-americano, considerado um dos maiores power forwards da história. Ele conquistou cinco títulos da NBA com o San Antonio Spurs e foi três vezes MVP das finais.",
+        link: "https://pt.wikipedia.org/wiki/Tim_Duncan"
+    },
+
+    {
+        titulo: "Dirk Nowitzki",
+        descricao: "Dirk Werner Nowitzki é um ex-jogador de basquete alemão, conhecido por seu arremesso de média distância. Ele conquistou um título da NBA com o Dallas Mavericks e foi eleito o MVP da temporada regular em 2007.",
+        link: "https://pt.wikipedia.org/wiki/Dirk_Nowitzki"
+    },
+
+    {
+        titulo: "Stephen Curry",
+        descricao: "Stephen Curry Wardell II é um jogador de basquete norte-americano, conhecido por sua habilidade de arremesso de três pontos. Ele conquistou quatro títulos da NBA com o Golden State Warriors e foi duas vezes MVP da temporada regular.",
+        link: "https://pt.wikipedia.org/wiki/Stephen_Curry"
+    },
+
+    {
+        titulo: "Hakeem Olajuwon",
+        descricao: "Hakeem Abdul Olajuwon é um ex-jogador de basquete nigeriano-americano, conhecido por sua habilidade defensiva e movimentos de pés. Ele conquistou dois títulos da NBA com o Houston Rockets e foi duas vezes MVP das finais.",
+        link: "https://pt.wikipedia.org/wiki/Hakeem_Olajuwon"
+    },
+
+    {
+        titulo: "Wilt Chamberlain",
+        descricao: "Wilton Norman Chamberlain foi um jogador de basquete norte-americano, conhecido por sua força física e números estatísticos impressionantes. Ele conquistou dois títulos da NBA e detém diversos recordes da liga.",
+        link: "https://pt.wikipedia.org/wiki/Wilt_Chamberlain"
+    },
+
+    {
+        titulo: "Julius Erving",
+        descricao: "Julius Winfield Erving II, mais conhecido como Dr. J, é um ex-jogador de basquete norte-americano, considerado um dos maiores jogadores de todos os tempos. Ele revolucionou o jogo com suas enterradas espetaculares e conquistou um título da NBA com o Philadelphia 76ers.",
+        link: "https://pt.wikipedia.org/wiki/Julius_Erving"
+    },
+
+    {
+        titulo: "Oscar Robertson",
+        descricao: "Oscar Palmer Robertson é um ex-jogador de basquete norte-americano, conhecido por sua versatilidade e estatísticas impressionantes. Ele foi o primeiro jogador a registrar um triplo-duplo de média por jogo em uma temporada e conquistou um título da NBA com o Milwaukee Bucks.",
+        link: "https://pt.wikipedia.org/wiki/Oscar_Robertson"
+    },
+
+    {
+        titulo: "Bill Walton",
+        descricao: "William Theodore Walton Jr. é um ex-jogador de basquete norte-americano, conhecido por sua habilidade de passar a bola e sua consciência defensiva. Ele conquistou um título da NBA com o Portland Trail Blazers e foi eleito o MVP da temporada regular em 1978.",
+        link: "https://pt.wikipedia.org/wiki/Bill_Walton"
+    },
+
+    {
+        titulo: "Karl Malone",
+        descricao: "Karl Anthony Malone é um ex-jogador de basquete norte-americano, conhecido por sua força física e habilidade de pontuar. Ele foi duas vezes MVP da temporada regular e conquistou dois títulos olímpicos com a seleção dos Estados Unidos.",
+        link: "https://pt.wikipedia.org/wiki/Karl_Malone"
+    },
+
+    {
+        titulo: "David Robinson",
+        descricao: "David Maurice Robinson é um ex-jogador de basquete norte-americano, conhecido por sua ética de trabalho e versatilidade. Ele conquistou dois títulos da NBA com o San Antonio Spurs e foi eleito o MVP da temporada regular em 1995.",
+        link: "https://pt.wikipedia.org/wiki/David_Robinson"
+    }
+
+];
