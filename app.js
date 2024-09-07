5b5bc7d332aac04a85d6d8c18269fb657e8e2e49
Arthur Moraes <133591851+ArtthurMoraes@users.noreply.github.com>
2024-09-06 23:15:19 -0300

    Tratando os possíveis erros no campo de pesquisa app.js
    Foram adicionadas estruturas condicionais afim de mitigar os possíveis erros que poderiam ser apresentados ao executar uma busca. Nesta atualização, tratei os valores que são passados na busca (tratamento de strings) e a mensagem de retorno que deve ser exibida quando o usuário não preencher o campo ou buscar um jogador não existente na base de dados.

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
+// Esta função é responsável por realizar uma pesquisa e exibir os resultados na página.
 function pesquisar() {
-    // Esta função é responsável por realizar uma pesquisa e exibir os resultados na página.
     
     // Obtém a seção onde os resultados serão exibidos pelo seu ID.
     let section = document.getElementById("resultados-pesquisa");
 
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Para encontrar um jogador, preencha o campo acima!</p>"
+        return 
+    }
+
+    //Transforma em letra minúscula tudo o que for digitado no campo de pesquisa
+    campoPesquisa = campoPesquisa.toLowerCase()
+
     // Inicializa uma string vazia para armazenar os resultados da pesquisa.
     let resultados = '';
+    let titulo = '';
+    let descricao = '';
+    let tags = '';
 
     // Itera sobre cada elemento do array 'dados' (que não foi mostrado no código).
     // Para cada elemento, cria um novo elemento HTML 'div' com as informações do dado.
-    for (let dado of dados){ 
-        resultados += `
+    
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+
+        //Verifica se a palavra digitada está contida na base de dados
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            //Cria um novo elemento
+            resultados += `
             <div class="item-resultado">
                 <h2>
                     <a href="#" target="_blank">${dado.titulo}</a>
@@ -20,9 +41,15 @@ function pesquisar() {
                 <a href=${dado.link} target="_blank">Saiba mais</a>
             </div>
         `;
+        }
+    }
+
+    if (!resultados) {
+        resultados = "<p>Nenhum resultado foi encontrado!</p>"
     }
 
     // Atribui o conteúdo da string 'resultados' ao elemento HTML 'section'.
     // Isso substitui o conteúdo anterior da seção pelos novos resultados da pesquisa.
     section.innerHTML = resultados;
+
 }
