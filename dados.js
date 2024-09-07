0319df25dee0dd9703f45de4d5b4cf8b59075487
Arthur Moraes <133591851+ArtthurMoraes@users.noreply.github.com>
2024-09-06 23:08:29 -0300

    Incluindo tags no objeto dados.js
    Adicionando a propriedade "tag" no objeto dados, facilitando a busca do usuário quando utilizar palavras chave, como apelido ou característica do jogador.

--- dados.js
+++ dados.js
@@ -2,111 +2,191 @@ let dados = [
     {
         titulo: "Michael Jordan",
         descricao: "Michael Jeffrey Jordan, nascido em 17 de fevereiro de 1963, é amplamente considerado o maior jogador de basquete de todos os tempos. Ele conquistou seis títulos da NBA com o Chicago Bulls e foi eleito o MVP das finais em todas essas ocasiões. Jordan também foi cinco vezes MVP da temporada regular e participou de 14 NBA All-Star Games.",
-        link: "https://pt.wikipedia.org/wiki/Michael_Jordan"
+        link: "https://pt.wikipedia.org/wiki/Michael_Jordan",
+        tags: "goat, best, melhor, lenda, Chicago Bulls, 6 títulos, MVP, All-Star"
     },
 
 
     {
         titulo: "LeBron James",
         descricao: "LeBron Raymone James, nascido em 30 de dezembro de 1984, é um dos maiores jogadores de basquete de todos os tempos.Atualmente jogando pelo Los Angeles Lakers, ele conquistou quatro títulos da NBA e foi eleito o MVP das finais em todas essas ocasiões.LeBron também foi quatro vezes MVP da temporada regular e participou de 19 NBA All- Star Games.",
-        link: "https://pt.wikipedia.org/wiki/LeBron_James"
+        link: "https://pt.wikipedia.org/wiki/LeBron_James",
+        tags: "King James, Lakers, 4 títulos, MVP, All-Star, versátil, dominante"
     },
 
     {
         titulo: "Kobe Bryant",
         descricao: "Kobe Bean Bryant, nascido em 23 de agosto de 1978 e falecido em 26 de janeirode 2020, é considerado um dos maiores jogadores de basquete da história. Ele passou toda suacarreira de 20 anos no Los Angeles Lakers, conquistando cinco títulos da NBA e sendo eleito o MVP das finais duas vezes. Kobe também foi 18 vezes All-Star e MVP da temporada regular em 2008.",
-        link: "https://pt.wikipedia.org/wiki/Kobe_Bryant"
+        link: "https://pt.wikipedia.org/wiki/Kobe_Bryant",
+        tags: "Black Mamba, Lakers, 5 títulos, MVP, All-Star, clutch, lenda"
 
     },
 
     {
         titulo: "Kareem Abdul-Jabbar",
         descricao: "Lew Alcindor, mais conhecido como Kareem Abdul-Jabbar, é um ex-jogador de basquete norte-americano, considerado um dos maiores de todos os tempos. Ele é o maior pontuador da história da NBA e possui o recorde de mais títulos de MVP da temporada regular.",
-        link: "https://pt.wikipedia.org/wiki/Kareem_Abdul-Jabbar"
+        link: "https://pt.wikipedia.org/wiki/Kareem_Abdul-Jabbar",
+        tags: "maior pontuador, lenda, Lakers, Bucks, skyhook, MVP, All-Star"
     },
 
     {
         titulo: "Bill Russell",
         descricao: "William Felton Russell é um ex-jogador de basquete norte-americano, considerado um dos maiores defensores da história da NBA. Ele conquistou 11 títulos da NBA com o Boston Celtics, um recorde que até hoje permanece invicto.",
-        link: "https://pt.wikipedia.org/wiki/Bill_Russell"
+        link: "https://pt.wikipedia.org/wiki/Bill_Russell",
+        tags: "defensivo, Celtics, 11 títulos, lenda, rebotes, bloqueios, MVP"
     },
     {
         titulo: "Magic Johnson",
         descricao: "Earvin Magic Johnson Jr. é um ex-jogador de basquete norte-americano, conhecido por sua habilidade de passar a bola e liderar a equipe. Ele conquistou cinco títulos da NBA com o Los Angeles Lakers e foi um dos principais rivais de Larry Bird.",
-        link: "https://pt.wikipedia.org/wiki/Magic_Johnson"
+        link: "https://pt.wikipedia.org/wiki/Magic_Johnson",
+        tags: "defensivo, Celtics, 11 títulos, lenda, rebotes, bloqueios, MVP"
     },
 
 
     {
         titulo: "Larry Bird",
         descricao: "Lawrence Joseph Bird é um ex-jogador de basquete norte-americano, conhecido por sua habilidade de arremessar e liderança. Ele conquistou três títulos da NBA com o Boston Celtics e foi um dos principais rivais de Magic Johnson.",
-        link: "https://pt.wikipedia.com/wiki/Larry_Bird"
+        link: "https://pt.wikipedia.com/wiki/Larry_Bird",
+        tags: "defensivo, Celtics, 11 títulos, lenda, rebotes, bloqueios, MVP"
     },
 
     {
         titulo: "Shaquille O'Neal",
         descricao: "Shaquille Rashaun O'Neal é um ex-jogador de basquete norte-americano, conhecido por sua força física e domínio na pintura. Ele conquistou quatro títulos da NBA e foi três vezes MVP das finais.",
-        link: "https://pt.wikipedia.org/wiki/Shaquille_O'Neal"
+        link: "https://pt.wikipedia.org/wiki/Shaquille_O'Neal",
+        tags: "Shaq, dominante, Lakers, 4 títulos, MVP, All-Star, força"
     },
 
     {
         titulo: "Tim Duncan",
         descricao: "Timothy Theodore Duncan, conhecido como Tim Duncan, é um ex-jogador de basquete norte-americano, considerado um dos maiores power forwards da história. Ele conquistou cinco títulos da NBA com o San Antonio Spurs e foi três vezes MVP das finais.",
-        link: "https://pt.wikipedia.org/wiki/Tim_Duncan"
+        link: "https://pt.wikipedia.org/wiki/Tim_Duncan",
+        tags: "Spurs, fundamental, 5 títulos, MVP, All-Star, power forward, lenda"
     },
 
     {
         titulo: "Dirk Nowitzki",
         descricao: "Dirk Werner Nowitzki é um ex-jogador de basquete alemão, conhecido por seu arremesso de média distância. Ele conquistou um título da NBA com o Dallas Mavericks e foi eleito o MVP da temporada regular em 2007.",
-        link: "https://pt.wikipedia.org/wiki/Dirk_Nowitzki"
+        link: "https://pt.wikipedia.org/wiki/Dirk_Nowitzki",
+        tags: "Mavericks, arremesso, MVP, campeão, alemão, lenda"
     },
 
     {
         titulo: "Stephen Curry",
         descricao: "Stephen Curry Wardell II é um jogador de basquete norte-americano, conhecido por sua habilidade de arremesso de três pontos. Ele conquistou quatro títulos da NBA com o Golden State Warriors e foi duas vezes MVP da temporada regular.",
-        link: "https://pt.wikipedia.org/wiki/Stephen_Curry"
+        link: "https://pt.wikipedia.org/wiki/Stephen_Curry",
+        tags: "Warriors, arremesso de três, MVP, campeão, Splash Brother, revolucionário"
     },
 
     {
         titulo: "Hakeem Olajuwon",
         descricao: "Hakeem Abdul Olajuwon é um ex-jogador de basquete nigeriano-americano, conhecido por sua habilidade defensiva e movimentos de pés. Ele conquistou dois títulos da NBA com o Houston Rockets e foi duas vezes MVP das finais.",
-        link: "https://pt.wikipedia.org/wiki/Hakeem_Olajuwon"
+        link: "https://pt.wikipedia.org/wiki/Hakeem_Olajuwon",
+        tags: "Rockets, defesa, MVP, campeão, Dream Shake, lenda"
     },
 
     {
         titulo: "Wilt Chamberlain",
         descricao: "Wilton Norman Chamberlain foi um jogador de basquete norte-americano, conhecido por sua força física e números estatísticos impressionantes. Ele conquistou dois títulos da NBA e detém diversos recordes da liga.",
-        link: "https://pt.wikipedia.org/wiki/Wilt_Chamberlain"
+        link: "https://pt.wikipedia.org/wiki/Wilt_Chamberlain",
+        tags: "recordes, força, 100 pontos, MVP, campeão, lenda"
     },
 
     {
         titulo: "Julius Erving",
         descricao: "Julius Winfield Erving II, mais conhecido como Dr. J, é um ex-jogador de basquete norte-americano, considerado um dos maiores jogadores de todos os tempos. Ele revolucionou o jogo com suas enterradas espetaculares e conquistou um título da NBA com o Philadelphia 76ers.",
-        link: "https://pt.wikipedia.org/wiki/Julius_Erving"
+        link: "https://pt.wikipedia.org/wiki/Julius_Erving",
+        tags: "Dr. J, enterradas, 76ers, MVP, campeão, ABA, lenda"
     },
 
     {
         titulo: "Oscar Robertson",
         descricao: "Oscar Palmer Robertson é um ex-jogador de basquete norte-americano, conhecido por sua versatilidade e estatísticas impressionantes. Ele foi o primeiro jogador a registrar um triplo-duplo de média por jogo em uma temporada e conquistou um título da NBA com o Milwaukee Bucks.",
-        link: "https://pt.wikipedia.org/wiki/Oscar_Robertson"
+        link: "https://pt.wikipedia.org/wiki/Oscar_Robertson",
+        tags: "triplo-duplo, versátil, MVP, Bucks, campeão, lenda"
     },
 
     {
         titulo: "Bill Walton",
         descricao: "William Theodore Walton Jr. é um ex-jogador de basquete norte-americano, conhecido por sua habilidade de passar a bola e sua consciência defensiva. Ele conquistou um título da NBA com o Portland Trail Blazers e foi eleito o MVP da temporada regular em 1978.",
-        link: "https://pt.wikipedia.org/wiki/Bill_Walton"
+        link: "https://pt.wikipedia.org/wiki/Bill_Walton",
+        tags: "Trail Blazers, MVP, campeão, defesa, passes, lenda"
     },
 
     {
         titulo: "Karl Malone",
         descricao: "Karl Anthony Malone é um ex-jogador de basquete norte-americano, conhecido por sua força física e habilidade de pontuar. Ele foi duas vezes MVP da temporada regular e conquistou dois títulos olímpicos com a seleção dos Estados Unidos.",
-        link: "https://pt.wikipedia.org/wiki/Karl_Malone"
+        link: "https://pt.wikipedia.org/wiki/Karl_Malone",
+        tags: "força, MVP, Jazz, pontuador, olímpico, lenda"
     },
 
     {
         titulo: "David Robinson",
         descricao: "David Maurice Robinson é um ex-jogador de basquete norte-americano, conhecido por sua ética de trabalho e versatilidade. Ele conquistou dois títulos da NBA com o San Antonio Spurs e foi eleito o MVP da temporada regular em 1995.",
-        link: "https://pt.wikipedia.org/wiki/David_Robinson"
-    }
+        link: "https://pt.wikipedia.org/wiki/David_Robinson",
+        tags: "Spurs, MVP, campeão, ética de trabalho, versátil, lenda"
+    }, 
+
+    {
+        titulo: "Kevin Durant",
+        descricao: "Kevin Durant é um ala norte-americano conhecido por sua altura, habilidade de arremesso e versatilidade. Ele conquistou dois títulos da NBA e foi eleito o MVP das finais em duas ocasiões. Atualmente joga no Phoenix Suns.",
+        link: "https://pt.wikipedia.org/wiki/Kevin_Durant",
+        tags: "Slim Reaper, arremesso, MVP, campeão, Nets, Suns, versátil, lenda"
+    },
+
+    {
+        titulo: "Giannis Antetokounmpo",
+        descricao: "Giannis Antetokounmpo, também conhecido como The Greek Freak, é um ala-pivô grego-nigeriano, considerado um dos melhores jogadores da atualidade. Ele conquistou um título da NBA com o Milwaukee Bucks e foi eleito o MVP da temporada regular em duas ocasiões.",
+        link: "https://pt.wikipedia.org/wiki/Giannis_Antetokounmpo",
+        tags: "Greek Freak, Bucks, MVP, campeão, defesa, versátil, lenda"
+    },
+
+    {
+        titulo: "James Harden",
+        descricao: "James Harden é um armador norte-americano conhecido por sua habilidade de arremesso de três pontos e criar jogadas para seus companheiros. Atualmente joga no Philadelphia 76ers.",
+        link: "https://pt.wikipedia.org/wiki/James_Harden",
+        tags: "Barba, arremesso de três, MVP, 76ers, assistências, All-Star"
+    },
+
+    {
+        titulo: "Luka Dončić",
+        descricao: "Luka Dončić é um armador esloveno, considerado um dos maiores jovens talentos da NBA. Conhecido por sua visão de jogo e habilidade de pontuar, ele já conquistou diversos prêmios individuais.",
+        link: "https://pt.wikipedia.org/wiki/Luka_Dončić",
+        tags: "Luka Magic, Mavericks, jovem talento, arremesso, visão de jogo, All-Star, esloveno"
+    },
+
+    {
+        titulo: "Anthony Edwards",
+        descricao: "Anthony Edwards é um ala norte-americano, conhecido por sua explosividade e habilidade de finalizar jogadas. Ele foi eleito o Rookie do Ano da NBA em 2021.",
+        link: "https://en.wikipedia.org/wiki/Anthony_Edwards_(basketball)",
+        tags: "Ant man, explosivo, Timberwolves, Rookie do Ano, finalizador, jovem talento"
+    }, 
+    {
+        titulo: "Ja Morant",
+        descricao: "Ja Morant é um armador explosivo conhecido por suas enterradas espetaculares e habilidade de finalizar jogadas. Ele lidera o Memphis Grizzlies e é considerado um dos maiores jovens talentos da NBA.",
+        link: "https://pt.wikipedia.org/wiki/Ja_Morant",
+        tags: "Grizzlies, enterradas, armador, jovem talento, explosivo, All-Star"
+    },
+
+    {
+        titulo: "Victor Wembanyama",
+        descricao: "Victor Wembanyama é um jovem jogador francês considerado uma das maiores promessas do basquete mundial. Com sua altura e versatilidade, ele é comparado a alguns dos maiores jogadores da história.",
+        link: "https://en.wikipedia.org/wiki/Victor_Wembanyama",
+        tags: "promessa, gigante, francês, altura, versatilidade, futuro, jovem talento"
+    },
+
+    {
+        titulo: "Nikola Jokić",
+        descricao: "Nikola Jokić é um pivô sérvio conhecido por sua visão de jogo, habilidade de passar a bola e habilidade de pontuar. Ele lidera o Denver Nuggets e foi eleito o MVP da temporada regular em duas ocasiões.",
+        link: "https://pt.wikipedia.org/wiki/Nikola_Jokić",
+        tags: "Joker, Nuggets, MVP, passes, pivô, visão de jogo, sérvio, All-Star"
+    },
+
+    {
+        titulo: "Kyrie Irving",
+        descricao: "Kyrie Irving é um armador conhecido por sua habilidade de driblar e arremessar. Ele conquistou um título da NBA com o Cleveland Cavaliers e é considerado um dos melhores manejadores de bola da história.",
+        link: "https://pt.wikipedia.org/wiki/Kyrie_Irving",
+        tags: "dribles, arremesso, campeão, All-Star, manejador de bola"
+    },
 
 ];
